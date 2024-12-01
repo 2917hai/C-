2ea70dda0d87ef0fda609b158cac3f33c51b2a64
Umesh Yadav <149967047+2917hai@users.noreply.github.com>
2024-12-01 12:52:58 +0530

    Add files via upload

---
+++ Module.cpp
@@ -0,0 +1,14 @@
+// module is the set of function
+// function is set of task with a name
+
+# include <iostream>
+using namespace std;
+void disp();
+int main()
+{
+    disp();
+}
+void disp()
+{
+    cout<<"this is first function"<<endl;
+}
